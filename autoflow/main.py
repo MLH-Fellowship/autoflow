88245c9791727ff49ff0ca89bedb26ed51ad6e0d
Dipanwita <susiejojo1@gmail.com>
2020-11-20 04:27:08 +0530

    Create new repo and add colab using Github API

--- autoflow/main.py
+++ autoflow/main.py
@@ -1,5 +1,6 @@
 import click
 from autoflow.scripts.new import new
+from autoflow.scripts.git_connect import git_connect
 
 # the main entry point for entry command
 @click.group()
@@ -11,10 +12,28 @@ def main():
 def hello():
     click.echo('Hello')
 
+# commands for github
+@click.command()
+@click.argument('acc_token', envvar='GITHUB_TOKEN')
+def git(acc_token):
+    # click.echo("git")
+    click.echo('Taking your Access token from the current env...')
+    git_obj = git_connect(acc_token)
+    if (click.confirm("Do you want to create a new repo?")):
+        repo_name = click.prompt("Enter repo name",type=str)
+        git_obj.create_repo(repo_name)
+    else:
+        if (click.confirm("Do you want to link to an existing repository?")):
+            repo_name = click.prompt("Enter repo name",type=str)
+            git_obj.existing_repo(repo_name)
+    if (click.confirm("Let us add your teammates as collaborators!")):
+        colab_name = click.prompt("Enter the Github handle of collaborator you want to add",type=str)
+        git_obj.add_colabs(colab_name)
 
 # adds command for the cli
 main.add_command(hello)
 main.add_command(new)
+main.add_command(git)
 
 #calls main function once the script runs
 if __name__ == '__main__':
---
+++ autoflow/scripts/git_connect.py
@@ -0,0 +1,13 @@
+from github import Github
+
+class git_connect:
+    def __init__(self,acc_token):
+        self.g = Github(acc_token)
+        self.user = self.g.get_user()
+    def create_repo(self,repo_name):
+        self.repo = self.user.create_repo(repo_name)
+    def existing_repo(self,repo_name):
+        repo_path = str(self.user.login)+"/"+repo_name
+        self.repo = self.g.get_repo(repo_path)
+    def add_colabs(self,colab_name):
+            self.repo.add_to_collaborators(colab_name,permission="push")
