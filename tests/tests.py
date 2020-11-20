1bf21f4eb605b0183dc4d8ae26aa0590a5ce6f84
Shilpita Biswas <51776663+sh-biswas@users.noreply.github.com>
2020-11-19 16:42:52 -0800

    Started new worflow and test script

---
+++ tests/tests.py
@@ -0,0 +1,16 @@
+from click.testing import CliRunner
+from autoflow.main import new, hello
+
+
+def test_new():
+    runner = CliRunner()
+    result = runner.invoke(new)
+    assert result.exit_code == 0
+    assert result.output.rstrip("\n") == '🔥 creates new project'
+
+
+def test_hello():
+    runner = CliRunner()
+    result = runner.invoke(hello)
+    assert result.exit_code == 0
+    assert result.output.rstrip("\n") == 'Hello'
