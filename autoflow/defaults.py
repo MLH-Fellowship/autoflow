de7b17dd675f38cc531c2754631e17405c757d60
Shilpita Biswas <51776663+sh-biswas@users.noreply.github.com>
2020-11-22 16:37:03 -0800

    Fix module links

--- autoflow/defaults.py
+++ autoflow/defaults.py
@@ -1,8 +1,8 @@
 from pathlib import Path
 
-#defines home as default directory for projects
+# defines home as default directory for projects
 defaultDirectory = str(Path.home())
-#defines nano as default editor for projects
+# defines nano as default editor for projects
 defaultTextEditor = 'nano'
-#defines a secret token from Github for github token
+# defines a secret token from Github for github token
 github_token = '${{secrets.GITHUB_TOKEN}}'
--- autoflow/env.py
+++ autoflow/env.py
@@ -4,53 +4,53 @@ from pathlib import Path
 from autoflow.defaults import defaultDirectory, defaultTextEditor, github_token
 from autoflow import readme_template
 
-#selects slash type depending on OS
+# selects slash type depending on OS
 slash = ''
 if os.name == 'posix':
     slash = '/'
 else:
     slash = '\\'
 
-#defines name of the global config folder
+# defines name of the global config folder
 configFolderName = '.autoflow'
-#defines name of the config file
+# defines name of the config file
 configFileName = 'af-config.json'
 
-#path for home directory
+# path for home directory
 home = str(Path.home())
-#path for .autoflow folder
+# path for .autoflow folder
 configFolder = home + slash + configFolderName
-#filepath for af-config
+# filepath for af-config
 configFilePath = configFolder + slash + configFileName
 
-#creates a dict for default data
+# creates a dict for default data
 defaultData = {
     'defaultDirectory': defaultDirectory,
     'defaultTextEditor': defaultTextEditor,
     'github_token': github_token
 }
 
-#contains the default directory of projects
+# contains the default directory of projects
 projectsDir = ''
 
-#checks if autoflow config folder exists
+# checks if autoflow config folder exists
 isDir = os.path.isdir(configFolder)
 
-#creates a folder if it doesn't
+# creates a folder if it doesn't
 if not isDir:
     os.mkdir(configFolder)
 
-#checks if af-config exists
+# checks if af-config exists
 isFile = os.path.isfile(configFilePath)
 
-#creates and adds default data if doesn't
+# creates and adds default data if doesn't
 if not isFile:
     with open(configFilePath,"w") as file:
         data = json.dumps(defaultData,sort_keys=True,indent=4)
         file.write(data)
         file.close()
         
-#opens af-config file to get global data
+# opens af-config file to get global data
 with open(configFilePath) as file:
     data = json.load(file)
     projectsDir = data['defaultDirectory']
