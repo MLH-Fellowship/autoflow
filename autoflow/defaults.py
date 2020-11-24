1fa93c5a1e67dcd7afa916a303f34daabff49ee0
Shilpita Biswas <51776663+sh-biswas@users.noreply.github.com>
2020-11-23 16:51:38 -0800

    Fix home path

--- autoflow/defaults.py
+++ autoflow/defaults.py
@@ -1,8 +1,8 @@
 from pathlib import Path
 
 # defines home as default directory for projects
-if str(Path.home()) == '/home/runner':
-    defaultDirectory = '/home/runner/work/autoflow/autoflow'
+if str(Path.home()) == '/home/runner' or str(Path.home()) == '/Users/runner':
+    defaultDirectory = '${{ GITHUB_WORKSPACE }}'
 else:
     defaultDirectory = str(Path.home())
 # defines nano as default editor for projects
