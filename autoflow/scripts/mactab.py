0fa1f41a0ea09b15fbdc3cd4f6423c8eda73386c
Shilpita Biswas <51776663+sh-biswas@users.noreply.github.com>
2020-11-23 19:06:22 -0800

    Fix terminal timeout

--- autoflow/scripts/mactab.py
+++ autoflow/scripts/mactab.py
@@ -10,15 +10,14 @@ def osascript(scpt):
 def openTab():
     script = f"""
             with timeout of 300 seconds
-                tell application "System Events"
-                    tell process "Terminal" to keystroke "t" using command down
-                end
-            with timeout of 300 seconds
-                tell application "Terminal"
-                    activate
-                    do script with command "cd {os.getcwd()}" in window 1
-                end tell
-                """
+            tell application "System Events"
+                tell process "Terminal" to keystroke "t" using command down
+            end
+            tell application "Terminal"
+                activate
+                do script with command "cd {os.getcwd()}" in window 1
+            end tell
+            """
     stdout, stderr = osascript(script)
     if stderr:
         sys.stderr.write('Error in Applescript: {}\n'.format(stderr))
