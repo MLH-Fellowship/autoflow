ac1924e616e2d093de29719292b2a0fbad375dbd
Saurabh Kumar Suryan <saurabhsuryan99@hotmail.com>
2020-11-26 23:19:06 +0530

    🔥 Updates setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,18 @@
-from os import name
 from setuptools import setup, find_packages
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
-    name='Autoflow',
-    version='0.1.0',
-    description='A package that automates your workflow',
+    name='python-af',
+    version='1.0.0',
+    description='A python package that automates your workflow',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/MLH-Fellowship/autoflow',
+    author='Team Autoflow',
+    platforms=['posix'],
+    license ='MIT',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
@@ -16,5 +24,15 @@ setup(
         'console_scripts': [
             'af = autoflow.main:main'
         ]
-    }
+    },
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Development Status :: 3 - Alpha",
+        "Programming Language :: Python :: 3.8",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: POSIX :: Linux",
+        "Topic :: Software Development :: Libraries :: Python Modules"
+    ],
+    keywords='autoflow, python-af, workflow automation, productivity',
+    python_requires='>=3.6',
 )
